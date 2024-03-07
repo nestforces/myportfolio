c019d0748462cffa59a65e349d2516a624e4b2c6
nestforces <maulanamahfud2113@gmail.com>
2024-03-07 15:09:49 +0700

    fix-minor-bugs

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -29,10 +29,10 @@ const BallWithImage = ({ imageSrc }) => {
     const pointLight = new THREE.PointLight(0xffffff, 1, 100);
     pointLight.position.set(0, 0, 50);
 
-    useFrame(() => {
-        // Rotate the ball around the y-axis
-        meshRef.current.rotation.y += 0.01;
-    });
+    // useFrame(() => {
+    //     // Rotate the ball around the y-axis
+    //     meshRef.current.rotation.y += 0.01;
+    // });
   
     return (
         <mesh ref={meshRef}>
@@ -86,7 +86,7 @@ const Skills = () => {
                     </Grid>
                     ) : (
                     <Text>Loading in desktop</Text>
-            )}
+                    )}
             </Box>
         </>
     )
